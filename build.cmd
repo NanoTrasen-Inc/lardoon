190cb146b1c59f6684f469d984a2a3c4d7a7996e
karel26 <dcs.specialk@gmail.com>
2023-03-18 11:08:47 +0100

    Added version info

--- build.cmd
+++ build.cmd
@@ -1,15 +1,22 @@
 @echo off
 set NODE_OPTIONS=--openssl-legacy-provider
+set GOARCH=386
 
 echo Cleaning up...
+del lardoon.exe 2>NUL
+del cmd\lardoon\resource.syso 2>NUL
 del /F dist\*.ico 2>NUL
 del /F dist\*.mp3 2>NUL
 del /F dist\*.LICENSE.txt 2>NUL
 del /F dist\*.js 2>NUL
 del /F dist\*.html 2>NUL
 del /F dist\*.css 2>NUL
+go install github.com/josephspurrier/goversioninfo/cmd/goversioninfo@latest
 call yarn
 call yarn build
 echo Building lardoon.exe ...
-go build -o lardoon.exe cmd\lardoon\main.go
+cd cmd\lardoon\
+go generate
+go build -o ..\..\lardoon.exe
+cd ..\..
 echo Done.
